06cd37bcaa8ebe0e1fce820702d00e55c81d3e9d
Jibril <jibrilf@lmtlss.dev>
2023-01-26 14:51:44 -0500

    Created read_file function.

---
+++ include/main.h
@@ -0,0 +1,9 @@
+#include <string>
+
+/*
+* read_file
+* Reads text currently stored in file and saves it in memory.
+*
+*/
+
+void read_file(std::string FILENAME);
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,25 @@
+#include <string> // strings
+#include <iostream> // read from files
+#include <fstream> // read from files
+
+using namespace std;
+
 int main(int argc, char *argv[]) {
+	// Open file from arguments or go to title screen
 
 	return 0;
+}
+
+void read_file(string FILENAME) {
+	// string to store each line
+	string text;
+	// file to read
+	ifstream TextFile(FILENAME);
+
+	// read the file line by line
+	while (getline(TextFile, text)) {
+		cout << text;
+	}
+
+	TextFile.close();
 }
