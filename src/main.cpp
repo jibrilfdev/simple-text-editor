30dc3d3a8409c2b2b5a89562472a1c1302de77e9
Jibril <jibrilf@lmtlss.dev>
2023-01-26 17:06:41 -0500

    Added program start logic.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,26 @@
 #include <string> // strings
 #include <iostream> // read from files
 #include <fstream> // read from files
+#include "../include/main.h"
 
 using namespace std;
 
+string CURRENT_FILE;
+
 int main(int argc, char *argv[]) {
 	// Open file from arguments or go to title screen
+	if (argc == 2) {
+		CURRENT_FILE = argv[1];
+		read_file(CURRENT_FILE);
+	} else if (argc > 2) {
+		cout << "Error: too many arguments." << endl;
+		return 1;
+	} else {
+		cout << "MAIN SCREEN HERE" << endl;
+		cout << "File name:";
+		cin >> CURRENT_FILE;
+		read_file(CURRENT_FILE);
+	}
 
 	return 0;
 }
@@ -18,7 +33,7 @@ void read_file(string FILENAME) {
 
 	// read the file line by line
 	while (getline(TextFile, text)) {
-		cout << text;
+		cout << text << endl;
 	}
 
 	TextFile.close();
